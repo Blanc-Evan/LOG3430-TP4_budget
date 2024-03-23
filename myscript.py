9a84723f4390c24104bd0ba28eddfb445091073f
Evan BLANC <evanblanc.pro@icloud.com>
2024-03-23 16:54:32 -0400

    [FIX] mysrcipt.py

--- myscript.py
+++ myscript.py
@@ -1,7 +1,8 @@
 import os
 
-badhash = input('Enter the bad commit hash: ')
-goodhash = input('Enter the good commit hash: ')
+badhash = 'c1a4be04b972b6c17db242fc37752ad517c29402'
+goodhash = 'e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c'
+
 test_command = 'python manage.py test'
 
 os.system(f'git bisect start {badhash} {goodhash}')
